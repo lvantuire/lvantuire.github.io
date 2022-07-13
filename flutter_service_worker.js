3a8dfd84b44e378209be0e78343ded6a5415710d
lvantuire <lvantuire@gmail.com>
2022-07-13 16:52:02 -0300

    block barras sup e inf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,9 +59,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bbe5e701b87d660269b8438779402a18",
-"/": "bbe5e701b87d660269b8438779402a18",
-"main.dart.js": "f5dfba77e81c9b901cc0f997aa7d60a8",
+"index.html": "89b86e16e9afc696161ae722d0b769cc",
+"/": "89b86e16e9afc696161ae722d0b769cc",
+"main.dart.js": "31369f01b404392ac7b24f94bc9bf147",
 "manifest.json": "ebe7baeffd305ed93c97c52183620745",
 "version.json": "db23ba1b186be3f8b460c5950bc1ffe2"
 };
